0095f1d6b1344634660a9848e154e852b72d1591
Nate <npzaldivar@gmail.com>
2023-01-16 12:31:41 -0700

    A few simple scripts to pratice variable use.

---
+++ chapter02/simple_message.py
@@ -0,0 +1,3 @@
+#Assign a message to a variable, and then print that message.
+simple_message = "This is a message for the variable."
+print(simple_message)
---
+++ chapter02/simple_messages.py
@@ -0,0 +1,6 @@
+#Assign a message to a variable, and print that message.  Then change the value of the variable to a new message, and print the new message.
+simple_message = "This is sample text for the variable."
+print(simple_message)
+
+simple_message = "This is a new sample text for that same variable."
+print(simple_message)
