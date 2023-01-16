edf5d4f6578c66c588f3b42716a473358ab3e653
Nate <npzaldivar@gmail.com>
2023-01-16 12:48:25 -0700

    Some simple files to practice using methods with variables.

---
+++ chapter02/name.py
@@ -0,0 +1,15 @@
+#Print a name 
+name = "Ada LoveLace"
+print(name)
+
+#Capitalize first letters
+name = "Ada LoveLace"
+print(name.title())
+
+#Capitalize entire string
+name = "ada lovelace"
+print(name.upper())
+
+#Make entire string lower-case
+name = "Ada LoveLace"
+print(name.lower())
