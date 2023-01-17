ed548b0cb75012038197a7a1261370eaa235757c
Nate <npzaldivar@gmail.com>
2023-01-17 11:04:43 -0700

    Scripts for practicing number operations in Python

---
+++ chapter02/assigning_multiple_variables_at_once.py
@@ -0,0 +1,3 @@
+x,y,z = 1,2,3
+print(f"x equals {x}\ny equals {y}\nz equals {z}")
+print(x,y,z)
---
+++ chapter02/number_eight.py
@@ -0,0 +1,5 @@
+#Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print() calls to see the results. You should create four lines that look like this:
+print(5+3)
+print(9-1)
+print(2*4)
+print(int(16/2)) #By using "int" I can change the result from being a float to being a normal integer. 
