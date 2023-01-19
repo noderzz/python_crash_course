509a8bb7986590777a5578de4185d1ed216e6508
Nate <npzaldivar@gmail.com>
2023-01-19 12:08:13 -0700

    Comparison/Statistic Practice

---
+++ chapter04/simple_stats.py
@@ -0,0 +1,10 @@
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+#Smallest number in the list
+print(min(digits))
+
+#Largest Number in the list
+print(max(digits))
+
+#The sum of all numbers in the list
+print(sum(digits))
