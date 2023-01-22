b20bcb424df616f9950c3c719b7337d396512708
Nate <npzaldivar@gmail.com>
2023-01-22 14:39:59 -0700

    Practice with Lists and Tuples

---
+++ chapter04/dimensions.py
@@ -0,0 +1,13 @@
+dimensions = (200,50)
+print(dimensions[0])
+
+print("\n")
+
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400,100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
---
+++ chapter04/test.py
@@ -0,0 +1,3 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+for i in players[2:]:
+    print(i.title())
