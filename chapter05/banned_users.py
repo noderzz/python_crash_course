b627e4cdac3decb64d97da55dc5fddb4af4f3ee6
Nate <npzaldivar@gmail.com>
2023-01-24 14:13:48 -0700

    Practice with Dictionaries

--- chapter05/banned_users.py
+++ chapter05/banned_users.py
@@ -1,5 +1,7 @@
+#Consider a list of users who are banned from commenting in a forum. You can check whether a user has been banned before allowing that person to submit a comment:
 banned_users = ['andrew', 'carolina', 'david']
 user = 'marie'
 
 if user not in banned_users:
     print(f"{user.title()}, you can post a response if you wish.")
+
---
+++ chapter06/alien_modify.py
@@ -0,0 +1,5 @@
+alien0={"color":"green"}
+print(f"The alien is the color {alien0['color']}.")
+
+alien0["color"]="yellow"
+print(f"The alien is the color {alien0['color'].title()}.")
