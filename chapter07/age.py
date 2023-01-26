62ff0c0f1a725fcd2617706dd0b4c2781d7aca3f
Nate <npzaldivar@gmail.com>
2023-01-26 14:28:07 -0700

    Practice with input() function.

---
+++ chapter07/age.py
@@ -0,0 +1,7 @@
+age = input("How old are you? ")
+age = int(age)
+
+if age >= 18:
+    print("You are old enough to ride.")
+else:
+    print("Sorry, you need to be at least 18 to ride.")
---
+++ chapter07/greeter_longer.py
@@ -0,0 +1,4 @@
+prompt = "If you tell us who you are, we can personlize the messages you see."
+prompt += "\nWhat is your first name?\n"
+name = input(prompt)
+print(f"Hello {name.title()}!")
