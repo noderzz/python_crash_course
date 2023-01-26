62e231251ac966bf1b7dc5d6d35fc450d14bedd8
Nate <npzaldivar@gmail.com>
2023-01-26 14:54:57 -0700

    Practice with While Loops

---
+++ chapter07/counting.py
@@ -0,0 +1,4 @@
+current_number = 1
+while current_number <= 50000:
+    print(current_number)
+    current_number += 1
---
+++ chapter07/parrot_while.py
@@ -0,0 +1,9 @@
+prompt = "Tell me something, and I will repeat it back to you."
+prompt += "\nEnter 'quit' to end the program.\n"
+
+message = ""
+
+while message != 'quit':
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
