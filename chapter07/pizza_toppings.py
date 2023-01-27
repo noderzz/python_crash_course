9294e40e4535b6de0d222e9fe7a5f837d2e2af7f
Nate <npzaldivar@gmail.com>
2023-01-26 22:21:33 -0700

    Small updates

--- chapter07/pizza_toppings.py
+++ chapter07/pizza_toppings.py
@@ -1,6 +1,6 @@
+#Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value. As they enter each topping, print a message saying you’ll add that topping to their pizza.
 pizza_toppings = "Please enter all the pizza toppings you want!:"
 pizza_toppings += "(Input 'quit' at anytime to end the program.)"
-
 toppings = []
 
 while True:
@@ -13,3 +13,4 @@ while True:
     else:
         toppings.append(topping.title())
         print(f"{topping.title()} has now been added to your pizza!")
+
