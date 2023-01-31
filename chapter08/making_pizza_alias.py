0e64627318b2fff1442f47ce821b769141b00260
Nate <npzaldivar@gmail.com>
2023-01-31 09:17:22 -0700

    Additional practice importing modules and practicing PEP8.

---
+++ chapter08/making_pizza_alias.py
@@ -0,0 +1,4 @@
+from pizza_module import make_pizza as mp
+
+mp('pepperoni')
+mp(12, 'mushrooms','green peppers','extra cheese')
---
+++ chapter08/making_pizzas_entire_alias.py
@@ -0,0 +1,5 @@
+import pizza_module as p
+
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushrooms','green peppers','extra cheese')
+
