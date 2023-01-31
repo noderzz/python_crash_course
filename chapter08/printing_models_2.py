57dc600ba1a3474e7e1090f59dab5eca455a288c
Nate <npzaldivar@gmail.com>
2023-01-31 09:25:28 -0700

    Completed last chatper 8 'Try it Yourself' section.

---
+++ chapter08/printing_models_2.py
@@ -0,0 +1,8 @@
+import printing_models_functions as p
+
+unprinted_designs = ['phone case','robot pendant','dodecahedron']
+completed_models = []
+
+p.print_models(unprinted_designs[:], completed_models)
+p.show_completed_models(completed_models)
+print(unprinted_designs)
