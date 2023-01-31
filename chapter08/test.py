4372d709074d6f88a799d568c654b821ebd4b2cd
Nate <npzaldivar@gmail.com>
2023-01-31 09:00:29 -0700

    Practice importing modules into code

--- chapter08/test.py
+++ chapter08/test.py
@@ -2,3 +2,8 @@ meat = ["beef", "fish", "pork", "chicken"]
 
 food = meat.pop(1)
 print(f"I like to eat {food}.")
+
+print('\n')
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[:3])
