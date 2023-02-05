5d14c4ab403a28f034fc55aca3990d671b4abbe9
Nate <npzaldivar@gmail.com>
2023-02-05 12:31:34 -0700

    Learning to sort classes.

--- chapter09/electric_car_battery.py
+++ chapter09/electric_car_battery.py
@@ -50,6 +50,16 @@ class Battery:
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-kWh battery.")
 
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+        
+        print(f"This car can go about {range} miles on a full charge.")
+
 my_leaf = ElectricCar("nissan", "leaf", 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
