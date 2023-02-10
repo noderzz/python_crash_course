3f7cfab18fc9f17ea277c4b26fe31505d40aca71
Nate <npzaldivar@gmail.com>
2023-02-09 20:46:39 -0700

    Practice with refractoring code.

--- chapter10/remember_me.py
+++ chapter10/remember_me.py
@@ -1,13 +1,32 @@
 from pathlib import Path
 import json
 
-path = Path('chapter10/username.json')
-if path.exists():
-    contents = path.read_text()
-    username = json.loads(contents)
-    print(f"Welcome back, {username}!")
-else:
+def get_stored_username(path):
+    """Get stored username if available."""
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        return username
+    else:
+        return None
+    
+def get_new_username(path):
+    """Prompt for a new username."""
     username = input("What is your name? ")
     contents = json.dumps(username)
     path.write_text(contents)
-    print(f"We'll remember you when you come back, {username.title()}!")
+    return username
+
+def greet_user():
+    """Greet the user by name."""
+    path = Path('chapter10/username.json')
+    username = get_stored_username(path)
+    if username:
+        print(f"Welcome back, {username.title()}!")
+    else:
+        username = input("What is your name? ")
+        contents = json.dumps(username)
+        path.write_text(contents)
+        print(f"We'll remember you when you come back, {username.title()}!")
+
+greet_user()
